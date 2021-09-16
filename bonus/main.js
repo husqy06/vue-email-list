c4acf9eda1c9f92d971fa1ee3554f4910a7a5130
husqy06 <luigf306@gmail.com>
2021-09-16 18:39:39 +0200

    Completamento bonus

---
+++ bonus/main.js
@@ -0,0 +1,19 @@
+Vue.config.devtools = true;
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        emails: [],
+        complete: false
+    },
+    mounted() {
+       
+        for(let i = 0; i < 10; i++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(randommail => {
+                    this.emails.push(randommail.data.response) 
+                })
+        }
+    }
+});
